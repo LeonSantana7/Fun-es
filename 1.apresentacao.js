7f26cce32fb4c4f11a8909675b97d7290f7b6788
Leon Santana <leon.santana854@gmail.com>
2023-10-19 14:36:21 -0300

    Introducao a funcao

---
+++ 1.apresentacao.js
@@ -0,0 +1,35 @@
+const pessoa = {
+    nome : "José",
+    idade: 30,
+    profissao: "professor",
+}
+if(pessoa.idade < 25){
+    console.log(`Meu nome é ${pessoa.nome}, sou um(a) jovem de ${pessoa.idade} anos 
+e sou ${pessoa.profissao}!`);
+}else if(pessoa.idade < 65) {
+    console.log(`Meu nome é ${pessoa.nome}, sou um(a) adulto ${pessoa.idade} anos 
+e sou ${pessoa.profissao}!`);
+} else {
+    console.log(`Meu nome é ${pessoa.nome}, sou um idoso de ${pessoa.idade} anos 
+e sou ${pessoa.profissao}!`);
+}
+
+
+
+const pessoa2 = {
+    nome : "Pedro",
+    idade: 65,
+    profissao: "médico",
+}
+
+console.log(`Meu nome é ${pessoa2.nome}, tenho ${pessoa2.idade} anos 
+e sou ${pessoa2.profissao}!`);
+
+const pessoa3 = {
+    nome : "Ana",
+    idade: 19,
+    profissao: "estudante",
+}
+
+console.log(`Meu nome é ${pessoa3.nome}, tenho ${pessoa3.idade} anos 
+e sou ${pessoa3.profissao}!`);
